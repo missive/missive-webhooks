4a48ace0bc6b3bbc19d60567d5abf60a41e41598
Etienne Lemay <etienne@heliom.ca>
2019-11-08 12:26:32 -0500

    More bulletproof invalid request handling

--- server.js
+++ server.js
@@ -22,11 +22,21 @@ app.post('/:provider', (req, res) => {
   let { provider } = req.params
   let integration, base64JSON = req.query.t
 
-  if (!base64JSON) {
-    return res.status(404).send(`Integration not found: ${provider}`)
+  let notFound = () => {
+    res.status(404).send(`Integration not found: ${provider}`)
   }
 
-  let data = JSON.parse(Atob(base64JSON))
+  if (!base64JSON) {
+    return notFound()
+  }
+
+  let data = null
+  try {
+    data = JSON.parse(Atob(base64JSON))
+  } catch (e) {
+    return notFound()
+  }
+
   let { token, options } = data
   options || (options = {})
 
@@ -61,7 +71,7 @@ app.post('/:provider', (req, res) => {
     return res.status(200).send('Event type not implemented')
   }
 
-  res.status(404).send(`Integration not found: ${provider}`)
+  notFound()
 })
 
 app.post('/', (req, res) => {
